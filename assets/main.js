17d99f7e7aa3dd29295b1325a30a1a362961a6e6
NikitaKovalenko111 <nikitakovalenko7219@gmail.com>
2021-09-25 21:15:05 +0300

    ver. 1.0.0

--- assets/main.js
+++ assets/main.js
@@ -3,41 +3,15 @@ const resultText = document.querySelector('.result');
 const settingsButton = document.querySelector('.settings-button');
 const submitButton = document.querySelector('.submit');
 const darkthemeCheckbox = document.querySelector('#dark-theme');
-
-function darkTheme() {
-    localStorage.setItem('dark-theme', true);
-    document.querySelector('body').style.backgroundColor = 'black';
-    document.querySelector('h1').style.color = 'white';
-    document.querySelector('label').style.color = 'white';
-     document.querySelectorAll('button').forEach(function(item) {
-        item.style.background = "white";
-        item.style.color = "black";
-    });
-    document.querySelectorAll("p").forEach(function(item) {
-        item.style.color = 'white';
-    });
-}
+const removeauthorCheckbox = document.querySelector("#remove-author");
+const author = document.querySelector("#author");
 
 if (localStorage.getItem('dark-theme') == 'true') {
     darkTheme();
 }
-
-if (settingsButton) {
-    settingsButton.addEventListener('click', function() {
-        document.location.href = '/assets/settings.html';
-    });
-};
-
-if (submitButton) {
-    submitButton.addEventListener('click', function() {
-        if (darkthemeCheckbox) {
-            if (darkthemeCheckbox.checked) {
-                darkTheme(); 
-            }
-        }
-        document.location.href = '/assets/index.html';
-    });
-};
+if (localStorage.getItem('author') == 'false') {
+    author.style.display = "none";
+}
 
 function topclang(num) {
     let sIndex = num % 2;
@@ -61,15 +35,71 @@ function topclang(num) {
 let func = topclang();
 
 numberInput.addEventListener('change', function () {
-        if (numberInput.value == '') {
-            resultText.textContent = '';
+    if (numberInput.value == '') {
+        resultText.textContent = '';
+    }
+    else {
+        func = topclang(numberInput.value);
+        func();
+    }
+});
+
+function darkTheme() {
+    localStorage.setItem('dark-theme', true);
+    document.querySelector('body').style.backgroundColor = 'black';
+    document.querySelector('body').style.border = 'solid 1px white';
+    document.querySelector('body').style.padding = '15px';
+    document.querySelectorAll('h1').forEach(function(item) {
+        item.style.color = 'white';
+    });
+    document.querySelectorAll('label').forEach(function(item) {
+        item.style.color = 'white';
+    });
+     document.querySelectorAll('button').forEach(function(item) {
+        item.style.background = "white";
+        item.style.color = "black";
+        item.onmouseenter = function() {
+            item.style.cursor = 'pointer';
+            item.style.backgroundColor = "tomato";
         }
-        else {
-            func = topclang(numberInput.value);
-            func();
+        item.onmouseleave = function() {
+            item.style.backgroundColor = "white";
         }
     });
+    document.querySelectorAll("p").forEach(function(item) {
+        item.style.color = 'white';
+    });
+}
 
+function lightTheme() {
+    localStorage.removeItem('dark-theme');
+    document.querySelector('body').style.backgroundColor = 'white';
+    document.querySelector('body').style.border = 'none';
+    document.querySelector('body').style.padding = '0';
+    document.querySelectorAll('h1').forEach(function(item) {
+        item.style.color = 'black';
+    });
+    document.querySelectorAll('label').forEach(function(item) {
+        item.style.color = 'black';
+    });
+     document.querySelectorAll('button').forEach(function(item) {
+        item.style.background = "white";
+        item.style.color = "black";
+        item.onmouseenter = function() {
+            item.style.cursor = 'pointer';
+            item.style.backgroundColor = "tomato";
+        }
+        item.onmouseleave = function() {
+            item.style.backgroundColor = "white";
+        }
+    });
+    document.querySelectorAll("p").forEach(function(item) {
+        item.style.color = 'black';
+    });
+}
 
-
-
+if (settingsButton) {
+    settingsButton.addEventListener('click', function() {
+        document.location.href = '/assets/settings.html';
+    });
+};
